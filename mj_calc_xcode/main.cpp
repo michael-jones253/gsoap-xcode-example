9265b5329c0bf61413866aa5f4abdead54b344c4
Michael Jones <mikej253@me.com>
2015-03-27 13:06:39 +1100

    mj_calc_xcode client target building and running against geneva server.

--- mj_calc_xcode/main.cpp
+++ mj_calc_xcode/main.cpp
@@ -8,8 +8,48 @@
 
 #include <iostream>
 
+#include "soapcalcProxy.h"
+#include "calc.nsmap"
+
+const char server[] = "http://websrv.cs.fsu.edu/~engelen/calcserver.cgi";
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    return 0;
+
+  if (argc < 4)
+  { fprintf(stderr, "Usage: [add|sub|mul|div|pow] num num\n");
+    exit(0);
+  }
+  double a, b, result;
+  a = strtod(argv[2], NULL);
+  b = strtod(argv[3], NULL);
+  calcProxy calc;
+  calc.soap_endpoint = server;
+  switch (*argv[1])
+  { case 'a':
+      calc.add(a, b, &result);
+      break;
+    case 's':
+      calc.sub(a, b, &result);
+      break;
+    case 'm':
+      calc.mul(a, b, &result);
+      break;
+    case 'd':
+      calc.div(a, b, &result);
+      break;
+    case 'p':
+      calc.pow(a, b, &result);
+      break;
+    default:
+      fprintf(stderr, "Unknown command\n");
+      exit(0);
+  }
+  if (calc.error)
+    calc.soap_stream_fault(std::cerr);
+  else
+    printf("result = %g\n", result);
+  return 0;
 }
+
