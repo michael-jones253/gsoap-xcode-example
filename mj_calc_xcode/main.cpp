9be61243ccfdaa2816caf095afddbc9fde03b2bd
Michael Jones <mikej253@me.com>
2015-04-25 08:42:41 +1000

    Catch and display usage errors.

--- mj_calc_xcode/main.cpp
+++ mj_calc_xcode/main.cpp
@@ -36,11 +36,14 @@ int main(int argc, const char * argv[]) {
         { "power",   required_argument,      nullptr,           'p' },
         { NULL,    0,                 nullptr,           0 }
     };
-
-  if (argc < 4)
-  { fprintf(stderr, "Usage: [add|sub|mul|div|pow] num num\n");
-    exit(0);
-  }
+    
+    string const usageStr = "Usage: [add|sub|mul|div|pow] num num\n";
+    
+    if (argc < 4)
+    {
+        cerr << usageStr << endl;
+        exit(-1);
+    }
     
     char const* server = standaloneServer.c_str();
     
@@ -49,79 +52,82 @@ int main(int argc, const char * argv[]) {
     double result{};
     char operation{};
     
-    do {
-        auto ch = getopt_long(argc, (char* const *)(argv), "gcea:s:m:d:p:", longopts, nullptr);
-
-        switch (ch) {
-            case 'a':
-            case 's':
-            case 'm':
-            case 'd':
-            case 'p':
-                operation = ch;
-                a = stod(optarg);
-                
-                if (optind >= argc) {
-                    throw runtime_error("Missing value");
-                }
-                
-                if (argc > (optind + 1)) {
-                    throw runtime_error("Too many arguments");
-                }
-                
-                b = stod(argv[optind]);
-                break;
-                
-            case 'c':
-                server = cgiServer.c_str();
-                
-            case 'g':
-                server = gsoapServer.c_str();
-                break;
-                
-            case 'e':
-                server = eduServer.c_str();
-                
-            case -1:
-                throw runtime_error("Not enough arguments");
-                
-            default:
-                throw runtime_error("Unknown command");
-                break;
-        }
-    } while (operation == 0);
+    try {
+        
+        do {
+            auto ch = getopt_long(argc, (char* const *)(argv), "gcea:s:m:d:p:", longopts, nullptr);
+            
+            switch (ch) {
+                case 'a':
+                case 's':
+                case 'm':
+                case 'd':
+                case 'p':
+                    operation = ch;
+                    a = stod(optarg);
+                    
+                    if (optind >= argc) {
+                        throw runtime_error("Missing value");
+                    }
+                    
+                    if (argc > (optind + 1)) {
+                        throw runtime_error("Too many arguments");
+                    }
+                    
+                    b = stod(argv[optind]);
+                    break;
+                    
+                case 'c':
+                    server = cgiServer.c_str();
+                    
+                case 'g':
+                    server = gsoapServer.c_str();
+                    break;
+                    
+                case 'e':
+                    server = eduServer.c_str();
+                    
+                case -1:
+                    throw runtime_error("Not enough arguments");
+                    
+                default:
+                    throw runtime_error("Unknown command");
+                    break;
+            }
+        } while (operation == 0);
+    } catch (exception const& ex) {
+        cerr << ex.what() << usageStr << endl;
+        exit(-1);
+    }
     
-  calcProxy calc;
-  calc.soap_endpoint = server;
-  switch (operation) {
-      case 'a':
-      calc.add(a, b, &result);
-      break;
-    case 's':
-      calc.sub(a, b, &result);
-      break;
-    case 'm':
-      calc.mul(a, b, &result);
-      break;
-    case 'd':
-      calc.div(a, b, &result);
-      break;
-    case 'p':
-      calc.pow(a, b, &result);
-      break;
-    default:
-      fprintf(stderr, "Unknown command\n");
-      exit(0);
-  }
+    calcProxy calc;
+    calc.soap_endpoint = server;
+    switch (operation) {
+        case 'a':
+            calc.add(a, b, &result);
+            break;
+        case 's':
+            calc.sub(a, b, &result);
+            break;
+        case 'm':
+            calc.mul(a, b, &result);
+            break;
+        case 'd':
+            calc.div(a, b, &result);
+            break;
+        case 'p':
+            calc.pow(a, b, &result);
+            break;
+        default:
+            fprintf(stderr, "Unknown command\n");
+            exit(0);
+    }
     
-  if (calc.error)
-    calc.soap_stream_fault(std::cerr);
-  else
-    printf("result = %g\n", result);
+    if (calc.error)
+        calc.soap_stream_fault(std::cerr);
+    else
+        printf("result = %g\n", result);
     
-    // FIX ME catch and print usage:
-    // { fprintf(stderr, "Usage: [add|sub|mul|div|pow] num num\n");
-
-  return 0;
+    return 0;
 }
 
